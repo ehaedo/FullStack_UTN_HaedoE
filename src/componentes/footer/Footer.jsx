d671e7d7e0ada86b4fee3e490f00677cfc850650
unknown <emmanuelhaedo.utn@gmail.com>
2023-12-05 20:30:05 -0300

    arreglos json3

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="contenedor-footer">
       <p>Todos los derechos resevados. © 2023 Harry Potter.  Diseñado por:
-        <a href='linkedin.com/in/emmanuelhaedo'>Emmanuel Haedo. </a>
+        <a href='https://www.linkedin.com/in/emmanuelhaedo?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhC9q25WdQe26VnNRbtpBQA%3D%3D'>Emmanuel Haedo. </a>
       </p>
     </footer>
   );
